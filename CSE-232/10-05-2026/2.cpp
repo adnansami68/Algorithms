d1294ed02839048bd930785f1e4b185255f57ad9
Md. Adnan Sami <a.m.adnansami143@gmail.com>
2026-05-10 20:46:07 +0600

    Add C++ program to find maximum of two numbers

---
+++ CSE-232/10-05-2026/2.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n1 , n2;
+
+    cout<<"Enter a number :"<<endl;
+    cin>>n1;
+
+    cout<<"Enter a number:"<<endl;
+    cin>>n2;
+    
+    if(n2<n1){
+        cout<<"Maximum NUmber: "<<n1<<endl;
+    }else{
+        cout<<"Maximum Number: "<<n2<<endl;
+    }
+
+}
